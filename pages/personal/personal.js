d506fa4d083d6a9077a69a40e5c7a1e85c9a94a8
zhupeng <zhupeng>
2022-10-07 14:44:59 +0800

    调整模块入口限制

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -87,9 +87,32 @@ Page({
         })
         break;
       case 'resignApply':
-        wx.navigateTo({
-          url: '../../pages/resignApply/resignApply',
-        })
+        if (!userInfo.validation) {
+          wx.showModal({
+            title: '温馨提示',
+            content: '请先进行实名认证',
+            confirmText: '去实名',
+            success: (res)=> {
+              if (res.confirm) {
+                //未实名 进入实名页面
+                wx.navigateTo({
+                  url: '../../pages/authCenterNew/authCenterNew',
+                });
+              } 
+            }
+          })
+        } else {
+          if (userInfo.memberStatus !== 'PREPARE_JOB_RESIGN' && userInfo.memberStatus !== 'JOB_ON') {
+            wx.showToast({
+              title: '您还未入职',
+              icon: 'none',
+            });
+            return;
+          }
+          wx.navigateTo({
+            url: '../../pages/resignApply/resignApply',
+          });
+        }
         break;
       case 'feedback':
         wx.navigateTo({
